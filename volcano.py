fcb2fbcc26799efdf1356e7fbdcf89ed6dbe674a
AnnaSakuchanova <106249844+AnnaSakuchanova@users.noreply.github.com>
2022-07-05 16:08:23 +0300

    Update volcano.py

--- volcano.py
+++ volcano.py
@@ -1,16 +1,15 @@
+#Импорт нужных библиотек
 import cv2
 import numpy as np
 
+#Загружаем видео
 cap = cv2.VideoCapture('speed_car_slowww.mp4')
 
-fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-out = cv2.VideoWriter('vid5.avi',fourcc, 30.0, (1280,720), False)
-
+#Считываем кадры видео и делаем преобразование Кэнни
 while(cap.isOpened()):
     ret, frame = cap.read()
     if ret==True:
         frame = cv2.Canny(frame,100,200)
-        out.write(frame)
         cv2.imshow('frame',frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -18,5 +17,4 @@ while(cap.isOpened()):
         break
 
 cap.release()
-out.release()
 cv2.destroyAllWindows()
